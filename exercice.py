da487b043c6a06547c3630569c8f639537acf9e4
JolanLec <jolan.le-clainche@polymtl.ca>
2020-09-15 23:05:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,9 @@
 
 def is_even_len(string: str) -> bool:
     if len(string) % 2 ==0:
-        return true
+        return True
+    esle:
+        return 
 
 
 def remove_third_char(string: str) -> str:
